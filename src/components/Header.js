0b4e9b015ef65aa08dcb5962c0776a9c86edb424
Eva Persson <persson_eva@outlook.com>
2019-11-19 11:16:13 +0100

    add navbar with logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {Container, Row, Col } from 'react-bootstrap'
+import {Container, Row, Col, Image, Nav, Navbar, Form, NavDropdown, FormControl, Button} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import freakylogo from '../images/freakylogo.png'
 
 function Header () {
-  return(
-    <Container>
-    
-    </Container>
-  
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Image src={freakylogo} alt="freaky-logo" href="#home"></Image>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 export default Header;
